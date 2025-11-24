91762a8afec534df65186d272db5bb68cab1272b
yojung51123 <ak471125@naver.com>
2025-11-24 10:21:35 +0900

    UJU : BeginPlay() 함수에 디버깅용 로직 추가
    깃 리베이스를 배워보고자 팀원에 로컬 레포지토리에서 변경사항을 만들고 커밋 푸시

--- Source/TeamProject/MyActor.cpp
+++ Source/TeamProject/MyActor.cpp
@@ -8,3 +8,10 @@ AMyActor::AMyActor()
 	PrimaryActorTick.bCanEverTick = false;
 }
 
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Log, TEXT("Written by other."));
+}
+
--- Source/TeamProject/MyActor.h
+++ Source/TeamProject/MyActor.h
@@ -14,4 +14,6 @@ class TEAMPROJECT_API AMyActor : public AActor
 public:
 	AMyActor();
 
+	virtual void BeginPlay() override;
+
 };
